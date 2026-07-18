bf4615b3249b81380d057db87e15be5f53b118f5
Olubulk <126447419+Olubulk@users.noreply.github.com>
2026-07-18 12:02:49 +0100

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // This allows production builds to successfully complete even if
+    // your project has TypeScript errors.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
